c96a64552019a98c82174afc207a1f0ac7403be3
luah5 <raymondvleeshouwer@gmail.com>
2023-04-17 10:07:57 +0100

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Raymond Vleeshouwer on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
