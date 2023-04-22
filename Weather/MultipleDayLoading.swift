1e96a6505ba173dd064f4a1bc990f66403058495
luah5 <raymondvleeshouwer@gmail.com>
2023-04-22 08:09:21 +0100

    fix remaining lint issues

--- Weather/MultipleDayLoading.swift
+++ Weather/MultipleDayLoading.swift
@@ -54,7 +54,6 @@ func multipleDays() {
     }
 }
 
-
 struct WeatherDescription {
     var mainDescription: String, description: String
 
@@ -66,23 +65,23 @@ struct WeatherDescription {
 
 // TODO: Implement the remaining fields
 struct WeatherHour {
-    var wind_gust: Float, pressure: Int, temp: Float, clouds: Int, dew_point: Float, visibility: Int
-    var dt: Int, humidity: Int, feels_like: Float, uvi: Int, wind_deg: Int, wind_speed: Float, weather: WeatherDescription
+    var windGust: Float, pressure: Int, temp: Float, clouds: Int, dewPoint: Float, visibility: Int, time: Int
+    var humidity: Int, feelsLike: Float, uvi: Int, windDeg: Int, windSpeed: Float, weather: WeatherDescription
 
     init(json: JSON) {
-        wind_gust = Float(json["wind_gust"].stringValue) ?? -100
+        windGust = Float(json["wind_gust"].stringValue) ?? -100
         pressure = Int(json["pressure"].stringValue) ?? -100
         temp = Float(json["temp"].stringValue) ?? -100
         clouds = Int(json["clouds"].stringValue) ?? -100
-        dew_point = Float(json["dew_point"].stringValue) ?? -100
+        dewPoint = Float(json["dew_point"].stringValue) ?? -100
         visibility = Int(json["visibilty"].stringValue) ?? -100
+        time = Int(json["dt"].stringValue) ?? -100
 
-        dt = Int(json["dt"].stringValue) ?? -100
         humidity = Int(json["humidity"].stringValue) ?? -100
-        feels_like = Float(json["feels_like"].stringValue) ?? -100
+        feelsLike = Float(json["feels_like"].stringValue) ?? -100
         uvi = Int(json["uvi"].stringValue) ?? -100
-        wind_deg = Int(json["wind_deg"].stringValue) ?? -100
-        wind_speed = Float(json["wind_speed"].stringValue) ?? -100
+        windDeg = Int(json["wind_deg"].stringValue) ?? -100
+        windSpeed = Float(json["wind_speed"].stringValue) ?? -100
 
         weather = WeatherDescription(json: json["weather"][0])
     }
