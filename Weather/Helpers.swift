3fc0072b4a7a14cee941347c6c3cf9ee0af08b8d
luah5 <raymondvleeshouwer@gmail.com>
2023-04-25 21:50:47 +0100

    code cleanup

--- Weather/Helpers.swift
+++ Weather/Helpers.swift
@@ -167,19 +167,3 @@ func tempDetailView(day: WeatherDay) -> some View {
         .frame(height: 50)
     }
 }
-
-struct PreviewColor: View {
-    private var color: Color
-    private var width: CGFloat
-
-    init(_ color: Color, width: CGFloat) {
-        self.color = color
-        self.width = width
-    }
-
-     var body: some View {
-        color
-            .frame(width: width, height: 5)
-            .cornerRadius(5)
-    }
-}
